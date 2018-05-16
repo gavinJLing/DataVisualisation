1213d8fc34f27144c65904a8a203ef965797dad9
gavinJLing <gavin@lingzone.co.uk>
2018-05-16 19:47:03 +0100

    Modified the REST response to return XML This was simple done by adapting the PerlinJSON project Jackson serialisation see build gradle - no java code changes made.

---
+++ DataSource/perlinXML/src/main/java/co/uk/lingzone/datasource/perlin/controller/RESTController.java
@@ -0,0 +1,69 @@
+package co.uk.lingzone.datasource.perlin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.uk.lingzone.datasource.perlin.dto.Data3D;
+import co.uk.lingzone.datasource.perlin.service.Data3DService;
+
+
+@RestController
+public class RESTController {
+    
+    @Autowired
+    private Data3DService data3DService;
+    
+    private int defaultMinCount = 1;
+    private double defaultMinStep = 0.0001D;
+    
+    
+    @RequestMapping("/ping")
+    public String ping() {
+        return "Hi";
+    }
+
+    
+    
+    
+    /*
+     * Default to a small grid of x,y,z Perlin values
+     */
+    @RequestMapping("/perlin")
+    public Data3D generate(@RequestParam(value = "xOffset", defaultValue = "0.5") double xOffset,
+            @RequestParam(value = "xStep", defaultValue = "0.01") double xStep,
+            @RequestParam(value = "xCount", defaultValue = "3") int xCount,
+
+            @RequestParam(value = "yOffset", defaultValue = "0.5") double yOffset,
+            @RequestParam(value = "yStep", defaultValue = "0.01") double yStep,
+            @RequestParam(value = "yCount", defaultValue = "3") int yCount,
+
+            @RequestParam(value = "zOffset", defaultValue = "0.3") double zOffset,
+            @RequestParam(value = "zStep", defaultValue = "0.01") double zStep,
+            @RequestParam(value = "zCount", defaultValue = "1") int zCount) {
+
+        // validate range values to ensure min. values
+        xCount = xCount< defaultMinCount ? defaultMinCount : xCount;
+        yCount = yCount< defaultMinCount ? defaultMinCount : yCount;
+        zCount = zCount< defaultMinCount ? defaultMinCount : zCount;
+        
+        xStep = Double.compare(xStep,defaultMinStep)==0 ? defaultMinStep : yStep;
+        yStep = Double.compare(yStep,defaultMinStep)==0 ? defaultMinStep : yStep;
+        zStep = Double.compare(zStep,defaultMinStep)==0 ? defaultMinStep : zStep;
+        
+        xOffset = xOffset < -1 ? -1 : xOffset;
+        xOffset = xOffset >  1 ?  1 : xOffset;
+        yOffset = yOffset < -1 ? -1 : yOffset;
+        yOffset = yOffset >  1 ?  1 : yOffset;
+        zOffset = zOffset < -1 ? -1 : zOffset;
+        zOffset = zOffset >  1 ?  1 : zOffset;
+        
+        Data3D data = data3DService.generate3dnoise(xOffset, xStep, xCount, yOffset, yStep, yCount, zOffset, zStep, zCount);
+
+        System.out.println(data);
+
+        return data;
+    }
+	
+}
---
+++ DataSource/perlinXML/src/main/java/co/uk/lingzone/datasource/perlin/dto/DataValues.java
@@ -0,0 +1,59 @@
+package co.uk.lingzone.datasource.perlin.dto;
+
+public class DataValues {
+    private Double xVal = 0.00D;
+    private Double yVal = 0.00D;
+    private Double zVal = 0.00D;
+    private Double noiseVal = 0.00D;
+
+    public DataValues(double x, double y, double z, double noise) {
+        xVal = x;
+        yVal = y;
+        zVal = z;
+        noiseVal = noise;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("x:").append(xVal).append(", ");
+        sb.append("y:").append(yVal).append(", ");
+        sb.append("z:").append(zVal).append(", ");
+        sb.append("n:").append(noiseVal).append("\n");
+        return sb.toString();
+    }
+
+    public Double getxVal() {
+        return xVal;
+    }
+
+    public void setxVal(Double xVal) {
+        this.xVal = xVal;
+    }
+
+    public Double getyVal() {
+        return yVal;
+    }
+
+    public void setyVal(Double yVal) {
+        this.yVal = yVal;
+    }
+
+    public Double getzVal() {
+        return zVal;
+    }
+
+    public void setzVal(Double zVal) {
+        this.zVal = zVal;
+    }
+
+    public Double getNoiseVal() {
+        return noiseVal;
+    }
+
+    public void setNoiseVal(Double noiseVal) {
+        this.noiseVal = noiseVal;
+    }
+
+}
